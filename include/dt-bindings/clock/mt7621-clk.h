15b2174da018a8e1ab036e4e20a47615d0c56a26
Chuanhong Guo <gch981213@gmail.com>
2019-04-10 17:16:18 +0800

    dt-bindings: clock: add dt binding header for mt7621-pll
    This patch adds dt binding header for mediatek,mt7621-pll
    
    Signed-off-by: Weijie Gao <hackpascal@gmail.com>
    Signed-off-by: Chuanhong Guo <gch981213@gmail.com>
    Reviewed-by: Rob Herring <robh@kernel.org>

---
+++ include/dt-bindings/clock/mt7621-clk.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2018 Weijie Gao <hackpascal@gmail.com>
+ */
+
+#ifndef __DT_BINDINGS_MT7621_CLK_H
+#define __DT_BINDINGS_MT7621_CLK_H
+
+#define MT7621_CLK_CPU		0
+#define MT7621_CLK_BUS		1
+
+#define MT7621_CLK_MAX		2
+
+#endif /* __DT_BINDINGS_MT7621_CLK_H */
