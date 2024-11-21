338b6e468873acdebde50e452c99988342705bce
Chuanhong Guo <gch981213@gmail.com>
2024-11-21 14:51:58 +0800

    dt-bindings: clock: add binding header for sf21-topcrm
    Add the device tree binding header for Siflower SF21A6826/SF21H8898
    toplevel clock and reset module. The header covers both clock and
    reset IDs provided by the block.
    
    Signed-off-by: Chuanhong Guo <gch981213@gmail.com>

---
+++ include/dt-bindings/clock/siflower,sf21-topcrm.h
@@ -0,0 +1,72 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR MIT) */
+
+#ifndef _DT_BINDINGS_CLK_SIFLOWER_SF21_TOPCRM_H
+#define _DT_BINDINGS_CLK_SIFLOWER_SF21_TOPCRM_H
+
+#define CLK_CMNPLL_VCO		0
+#define CLK_CMNPLL_POSTDIV	1
+
+#define CLK_DDRPLL_POSTDIV	2
+
+#define CLK_PCIEPLL_VCO		3
+#define CLK_PCIEPLL_FOUT0	4
+#define CLK_PCIEPLL_FOUT1	5
+#define CLK_PCIEPLL_FOUT2	6
+#define CLK_ETH_REF_P		CLK_PCIEPLL_FOUT2
+#define CLK_PCIEPLL_FOUT3	7
+
+#define CLK_CPU			8
+#define CLK_PIC			9
+#define CLK_AXI			10
+#define CLK_AHB			11
+#define CLK_APB			12
+#define CLK_UART		13
+#define CLK_IRAM		14
+#define CLK_NPU			15
+#define CLK_DDRPHY_REF		16
+#define CLK_DDR_BYPASS		17
+#define CLK_ETHTSU		18
+#define CLK_GMAC_BYP_REF	19
+
+#define CLK_USB			20
+#define CLK_USBPHY		21
+#define CLK_SERDES_CSR		22
+#define CLK_CRYPT_CSR		23
+#define CLK_CRYPT_APP		24
+#define CLK_IROM		25
+
+#define CLK_BOOT		26
+
+#define CLK_PVT			27
+#define CLK_PLL_TEST		28
+
+#define CLK_PCIE_REFN		29
+#define CLK_PCIE_REFP		30
+
+#define CLK_MAX			31
+
+#define SF21_RESET_GIC			0
+#define SF21_RESET_AXI			1
+#define SF21_RESET_AHB			2
+#define SF21_RESET_APB			3
+#define SF21_RESET_IRAM			4
+
+#define SF21_RESET_NPU			5
+#define SF21_RESET_DDR_CTL		6
+#define SF21_RESET_DDR_PHY		7
+#define SF21_RESET_DDR_PWR_OK_IN	8
+#define SF21_RESET_DDR_CTL_APB		9
+#define SF21_RESET_DDR_PHY_APB		10
+
+#define SF21_RESET_USB			11
+
+#define SF21_RESET_PVT			12
+#define SF21_RESET_SERDES_CSR		13
+
+#define SF21_RESET_CRYPT_CSR		14
+#define SF21_RESET_CRYPT_APP		15
+#define SF21_RESET_NPU2DDR_ASYNCBRIDGE	16
+#define SF21_RESET_IROM			17
+#define SF21_RESET_MAX			18
+
+#endif
